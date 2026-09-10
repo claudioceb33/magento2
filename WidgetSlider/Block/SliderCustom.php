84b0a957e1124a7e6fdb00ba0cb27e7b48a55b1d
claudioceb <claudio.ceb2@gmail.com>
2026-09-10 15:48:37 -0300

    Module WidgetSlider

---
+++ WidgetSlider/Block/SliderCustom.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ceb\WidgetSlider\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class SliderCustom extends Template implements BlockInterface
+{
+    protected $_template = "widget/slider-custom.phtml";
+}
